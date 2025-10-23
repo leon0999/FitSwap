e1378a77472c816e8bc3686c7717020abb73d770
Leon <leon0999@gmail.com>
2025-10-23 20:58:30 +0900

    Fix: JSX 구문 오류 수정 (</span> → </div>)

--- src/components/ResultsDisplay.tsx
+++ src/components/ResultsDisplay.tsx
@@ -390,7 +390,7 @@ function MapModal({ food, onClose }: MapModalProps) {
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                             </svg>
                             {formatDistance(restaurant.distance)}
-                          </span>
+                          </div>
                         )}
 
                         {/* 영업 여부 */}
